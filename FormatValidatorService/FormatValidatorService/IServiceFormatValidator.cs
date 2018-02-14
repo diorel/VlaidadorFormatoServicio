6c186f1a926cf7b4a797fd2168964a75f139c65b
Diorelyon Amador <diorel_x@hotmail.com>
2018-02-14 11:19:38 -0600

    Sevicio copia imagen
    servicio combierte imagen en binario y despues inmagen

---
+++ FormatValidatorService/FormatValidatorService/IServiceFormatValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FormatValidatorService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServiceFormatValidator
+    {
+        [OperationContract]
+        string VerificarIfe(string Foto);
+    }
+ 
+}
---
+++ FormatValidatorService/FormatValidatorService/ServiceFormatValidator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+
+namespace FormatValidatorService
+{
+   public class ServiceFormatValidator : IServiceFormatValidator
+    {
+
+        public string VerificarIfe(string Foto)
+        {
+
+            // crear un objeto imagen desde archivo
+
+            Image imagen = Image.FromFile(@"C:\Foto\Gundam1.jpg");
+
+            // Crar Un MemoryStream
+
+            var ms = new MemoryStream();
+
+            // salvar los bytes  en ms
+
+            imagen.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            // Obtenr los bytes
+
+            var bytes = ms.ToArray();
+
+            var imageMemoryStream = new MemoryStream(bytes);
+
+            Image imgFormStream = Image.FromStream(imageMemoryStream);
+
+            try
+            {
+
+                imgFormStream.Save(@"C:\Users\rcortes\Documents\GitHub\VlaidadorFormatoServicio\FormatValidatorService\FormatValidatorService\Foto\\Gundam2.jpg", ImageFormat.Jpeg);
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+
+    }
+}
